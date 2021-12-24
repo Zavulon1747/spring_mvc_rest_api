be2a47f8c0b8fabe9e984fc129bd3f62a06dfb34
Zavulon1747 <d1mas322@yandex.ru>
2021-12-24 19:54:09 +0400

    Global Exception Handler @ControllerAdvice

--- src/main/java/rest/controller/MyRESTController.java
+++ src/main/java/rest/controller/MyRESTController.java
@@ -1,11 +1,12 @@
 package rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import rest.entity.Employee;
+import rest.exception_handling.EmployeeIncorrectData;
+import rest.exception_handling.NoSuchEmployeeException;
 import rest.service.EmployeeService;
 
 import java.util.List;
@@ -26,7 +27,14 @@ public class MyRESTController {
     @GetMapping("/employees/{id}") //path variable
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = "+id+" in Database");
+        }
+
         return employee;
     }
+
+
 
 }
